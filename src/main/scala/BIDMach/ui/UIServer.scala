ed8c2383fc2c48182c52d71e545652e15e16d95b
byeah <jby.yeah@gmail.com>
2016-11-18 15:14:18 -0800

    cleanup useless code

--- src/main/scala/BIDMach/ui/UIServer.scala
+++ src/main/scala/BIDMach/ui/UIServer.scala
@@ -5,6 +5,8 @@ import BIDMat.MatFunctions._
 import BIDMat.SciFunctions._
 import BIDMach._
 import BIDMach.models._
+import BIDMach.datasources._
+import BIDMach.datasinks._
 
 import play.api._
 import play.api.routing.Router
@@ -15,24 +17,42 @@ import play.core.server.{ServerConfig, NettyServer}
 import play.api.mvc._
 import play.api.libs.iteratee._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+
 
 /**
  * The UIServer class for BIDMach Engine
  * It accepts a Learner and an Options as parameter and create the server for Interactive Machine Learning
  * 
  * 
- * */
+ **/
+ 
 
-class UIServer(nn: Learner,opts: BIDMat.Opts) extends LocalWebServer{
+class UIServer(nn: Learner,opts: Learner.Options) extends LocalWebServer{
+    
+    val modelName = nn.model.getClass.getSimpleName
+    val mopts = opts.asInstanceOf[Model.Opts]
+    val paras = utils.getOptions(opts)
+    var channel: Concurrent.Channel[String] = null
+    
     override def routes = {
         case GET(p"/")=>
           controllers.Assets.at(path="/public", file="index.html")
+        case GET(p"/meta")=>Action {
+            Results.Ok(JsObject(Seq(                
+                "type" -> JsString("meta"),
+                "model" -> JsString(modelName),
+                "parameters" -> JsObject(paras.map(x=>x._1 -> JsString(x._2)))
+            )))
+        }
+        case GET(p"/register/")=>Action {
+            Results.Ok("OK")
+        }
         case GET(p"/assets/$file*")=>
           controllers.Assets.at(path="/public", file=file)
         case GET(p"/socket")=>
            WebSocket.using[String] { 
                request => 
-           
                 // Concurrent.broadcast returns (Enumerator, Concurrent.Channel)
                 val (out, channel) = Concurrent.broadcast[String]
 
@@ -47,15 +67,16 @@ class UIServer(nn: Learner,opts: BIDMat.Opts) extends LocalWebServer{
                 (in,out)
               }
     }
-    
 }
 
 object UIServer {
-    def start(nn: Learner,opts: BIDMat.Opts) {
+    
+    def start(nn: Learner,opts: Learner.Options) {
         Mat.checkMKL
         Mat.checkCUDA
         val server = new UIServer(nn,opts)
     }
+    
     def main(args: Array[String]) {
         val (nn,opts) = KMeans.learner(rand(100,10000))
         start(nn,opts)
